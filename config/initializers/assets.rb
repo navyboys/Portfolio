941a311b6a3f3a214c710ea775d1fcf36a2b601d
navy <navyboys@gmail.com>
2016-03-09 11:44:24 -0800

    resotre assets pipeline back

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-<<<<<<< HEAD
 Rails.application.config.assets.precompile += %w( *.js *.css *.css.erb )
-=======
->>>>>>> parent of 4e126f0... add asset precompile setting to production
