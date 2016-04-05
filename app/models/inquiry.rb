0e1b19baf7e97fa3625391bc117a70a1f3318b7e
navy <navyboys@gmail.com>
2016-04-05 10:42:01 -0700

    debug: app crash on heroku

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,7 +2,8 @@ class Inquiry
   include ActiveAttr::Model
 
   attribute :name
-  attribute :email attribute :message
+  attribute :email
+  attribute :message
 
   validates_presence_of :name, :email, :message
   validates_format_of :email, with: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, multiline: true
